466f99faeec7eca8b2d2b5b25b37b7a07e47b8b8
bilocan-admin <m.bilalyaz@gmail.com>
2025-04-15 19:00:34 +0300

    Iyilestirmeler yapildi

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -29,16 +29,16 @@ export default function SocialLinks() {
         </svg>
       </a>
 
-      {/* Donanım Arşivi */}
+      {/* GitHub */}
       <a
-        href="https://forum.donanimarsivi.com/uyeler/b1l0.75952/#about"
+        href="https://github.com/B1L0CAN"
         target="_blank"
         rel="noopener noreferrer"
         className="text-gray-400 hover:text-blue-400 transition-colors"
-        title="Donanım Arşivi"
+        title="GitHub"
       >
-        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
+        <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
+          <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.387.6.113.82-.258.82-.577v-2.234c-3.338.726-4.033-1.415-4.033-1.415-.546-1.387-1.333-1.757-1.333-1.757-1.089-.745.083-.729.083-.729 1.205.084 1.838 1.237 1.838 1.237 1.07 1.834 2.809 1.304 3.495.997.108-.775.418-1.305.762-1.605-2.665-.3-5.467-1.334-5.467-5.931 0-1.31.469-2.381 1.236-3.221-.124-.303-.536-1.523.117-3.176 0 0 1.008-.322 3.3 1.23a11.5 11.5 0 0 1 6 0c2.291-1.552 3.297-1.23 3.297-1.23.655 1.653.243 2.873.12 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.629-5.479 5.922.43.372.823 1.104.823 2.222v3.293c0 .322.218.694.825.576C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/>
         </svg>
       </a>
     </div>
